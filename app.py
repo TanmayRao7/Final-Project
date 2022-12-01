710d869472f6f0f4c028f9005ec99dd4aec6ac3f
TanmayRao7 <rao_tanmay.it@ghrce.raisoni.net>
2022-12-02 00:52:03 +0530

    help code added for tickers

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 import os
 import matplotlib.pyplot as plt
+import os
 
 def get_data(symbol_ticker):
     company= symbol_ticker
@@ -22,6 +23,22 @@ def endOfDay(symbol_ticker):
     response = "Current Prices for "+ symbol_ticker.upper() +" in "+raw_text+" :\n"
     response += string.to_string()
     return response
+
+def get_Consolidated_data(symbol_ticker):
+    df = get_data(symbol_ticker)
+    df = yf.download(symbol_ticker , period="1d" , interval="1m")
+    trace1 = go.Scatter(x = df['Datetime'], y = df['Open'], name='Opening Prices (in Rupees)')
+    trace2 = go.Scatter(x = df['Datetime'], y = df['High'], name='High Prices (in Rupees)')
+    trace3 = go.Scatter(x = df['Datetime'], y = df['Low'], name='Low Prices (in Rupees)')
+    trace4 = go.Scatter(x = df['Datetime'], y = df['Close'], name='Closing Prices (in Rupees)')
+    trace5 = go.Scatter(x = df['Datetime'], y = df['Adj Close'], name='Adjusted Closing Prices (in Rupees)')
+    fig = go.Figure(data=[trace1, trace2, trace3, trace4,trace5] )
+    fig.update_layout(title='Current Stock Prices against Time',
+                   plot_bgcolor='rgb(230, 230,230)',
+                   showlegend=True)
+    if not os.path.exists("images"):
+        os.mkdir("images")
+    return fig
 
 app = Flask(__name__)
  
@@ -42,11 +59,21 @@ def wa_sms_reply():
     # Create reply
     if msg == "hi":
        reply.body("Hello, welcome to the Stock Market Bot \n Type sym:<stock_symbol> to know the price of the stock. ")
-    elif "sym:" in msg:
+    elif "sym:" in msg and "g:sym" not in msg:
         symbol = msg.split(":")
         symbol = str(symbol[1])
         response = endOfDay(symbol)
         reply.body(""+response)
+    elif "g:sym:" in msg :
+        symbol = msg.split(":")
+        symbol = str(symbol[2])
+        img = get_Consolidated_data(symbol)
+        reply.media(img)
+
+    elif "help" in msg:
+        reply.body("To Get Stock Codes : \n"+'https://github.com/TanmayRao7/Final-PR/blob/1c4176ac1b24fd4292be93ca12cbc19d3a336f6a/stock_tickers.pdf')
+    else:
+        reply.body("Please type hi to get started.")
 
     
 
